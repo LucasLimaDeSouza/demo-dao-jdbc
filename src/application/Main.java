b0dbfcd8b722219001b3d867ad18b19d6e39c011
LucasLimaDeSouza <lukxx23121996@gmail.com>
2024-09-12 12:01:08 -0300

    Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-
+        SimpleDateFormat sdf = new SimpleDateFormat();
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
+        Seller seller = new Seller(21, "Lucas", "lulu@gmail.com", new Date(),3000.0, obj);
+        System.out.println(seller);
     }
 }
